42e8492aba31294634bfd79c61882e883ab8b470
taylorburgues <theo_xone@outlook.com>
2022-11-16 22:24:49 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Menu from './components/template/Menu.js';
+import Main from './components/template/Main.js';
+import Footer from './components/template/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/template/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function(props) {
+    return(
+        <header className='header'>
+            Cabeçalho
+        </header>
+    )
+}
